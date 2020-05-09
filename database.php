2488a066f9ad927348578f3c1f595eddc057b48d
HongYen24 <leeeunyin9813@gmail.com>
2020-05-09 12:46:29 +0700

    Update database.php

--- database.php
+++ database.php
@@ -10,23 +10,17 @@ if($link === false){
 else{
   echo "Connection into Heroku PostGres successfully";
 }
-$id = $_REQUEST['product_ID'];
-$name = $_REQUEST['p_Name'];
-$cate = $_REQUEST['p_Cate'];
-$price = $_REQUEST['p_Price'];
-$desc = $_REQUEST['p_Des'];
+$username = $_REQUEST['Username'];
+$mess = $_REQUEST['Mess'];
 
-echo $id;
-echo $name;
-echo $cate;
-echo $price;
-echo $desc;
+echo $username;
+echo $mess;
 
 // Attempt insert query execution
-$sql4 = 'INSERT INTO public."Product" (
-  "product_ID","p_Name","p_Cate","p_Price","p_Des") 
-  VALUES ('." '$id'::character varying,'$name'::character varying,'$cate'::character varying,'$price'::integer,'$desc'::character varying)".' 
-  returning "product_ID"';
+$sql4 = 'INSERT INTO public."message" (
+  "Username","Mess") 
+  VALUES ('." '$username'::character varying,'$mess'::character varying)".' 
+  returning "Username"';
   if(pg_query($link, $sql4)){
     echo "Records added successfully.";
   } 
